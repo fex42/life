95785480430a599fbcdaeb5fbd7767af32b5d265
Felix Na <felix.na70@gmail.com>
2019-04-20 22:29:42 +0200

    make board non-quadratic

--- src/main/java/justme/GameOfLifeApplication.kt
+++ src/main/java/justme/GameOfLifeApplication.kt
@@ -4,13 +4,13 @@ import java.awt.EventQueue
 import javax.swing.JFrame
 
 class GameOfLifeApplication : JFrame() {
-    private val glider = Board(20, 20,
+    private val glider = Board(18, 22,
             hashSetOf(Pos(4, 4), Pos(5, 5), Pos(6, 5), Pos(6, 4), Pos(6, 3)))
 
     init {
         add(BoardPanel(glider))
 
-        setSize(300, 337)
+        setSize(360, 440+37)
 
         title = "Game of Life"
         defaultCloseOperation = EXIT_ON_CLOSE
